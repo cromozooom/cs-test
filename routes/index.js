a2383d9175ae149788988b336bdd38aba86e2251
cromozooom <cromozooom@gmail.com>
2017-01-12 17:36:36 +0000

    add files for test
    finish test

---
+++ routes/index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+
+var corporateJson = require('../data/corporate.json');
+var activeTradeJson = require('../data/activeTrade.json');
+var notesJson = require('../data/notes.json');
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  var corporateData     = corporateJson.corporate;
+  var activeTradeData   = activeTradeJson.activeTrade;
+  var notesData         = notesJson.notes;
+
+  console.log(notesData[1].notesdatemonth);
+
+  res.render('index', {
+    corporate:          corporateData,
+    activeTrade:        activeTradeData,
+    notes:              notesData,
+    title:              'Express'
+  });
+});
+
+module.exports = router;
